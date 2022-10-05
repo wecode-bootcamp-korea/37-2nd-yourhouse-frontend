85aa3a8108a6f20e938a5f08544fb272fb56f554
Jiwon <jiwony933@naver.com>
2022-10-05 19:52:39 +0900

    WIP

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <Container>로그인</Container>;
 };
 
 export default Login;
+
+const Container = styled.div``;
