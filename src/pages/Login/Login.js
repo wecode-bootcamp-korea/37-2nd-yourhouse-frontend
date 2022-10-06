2328bf6a680b74b5f0a4ac8daba8bb5454b0a821
Jiwon <jiwony933@naver.com>
2022-10-06 16:34:26 +0900

    ADD : 로그인/회원가입  레이아웃 및 통신 완료, 로그인 메시지 수정

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Login = () => {
-  return <div>Login</div>;
+  const API_KEY = process.env.REACT_APP_REST_API_KEY;
+  const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+
+  console.log(API_KEY);
+
+  console.log(REDIRECT_URI);
+
+  return (
+    <Container>
+      <FormWrap>
+        <LogoImage src="./images/logo.png" />
+        <Button>
+          <a
+            href={`https://kauth.kakao.com/oauth/authorize?client_id=${API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`}
+          >
+            카카오 계정으로 간편 로그인/회원가입
+          </a>
+        </Button>
+        <Text>로그인에 문제가 있으신가요?</Text>
+      </FormWrap>
+    </Container>
+  );
 };
 
 export default Login;
+
+const Container = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+`;
+
+const FormWrap = styled.div`
+  ${props => props.theme.variables.flex('column', 'center', 'center')};
+  ${props => props.theme.variables.absoluteCenter};
+  gap: 40px;
+  margin-top: 100px;
+`;
+
+const LogoImage = styled.img``;
+
+const Button = styled.button`
+  all: unset;
+  padding: 20px 30px;
+  background-color: #fae100;
+  font-size: 18px;
+  border-radius: ${props => props.theme.style.borderRadius};
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.7;
+  }
+`;
+
+const Text = styled.span`
+  color: ${props => props.theme.style.middleGrey};
+  cursor: pointer;
+`;
---
+++ src/pages/Login/UserAuth.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { RotatingLines } from 'react-loader-spinner';
+import { useLocation, useNavigate } from 'react-router-dom';
+
+const UserAuth = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const code = location.search;
+
+  useEffect(() => {
+    fetch(`http://10.58.52.78:3000/user/auth${code}`, {
+      headers: {
+        'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    })
+      .then(res => res.json())
+      .then(
+        result => (
+          result.accessToken
+            ? alert(`${result.nickname}님 반갑습니다!`)
+            : alert('로그인 실패'),
+          localStorage.setItem('token', result.accessToken),
+          localStorage.setItem('nickname', result.nickname)
+        )
+      )
+      .then(() => navigate('/main'));
+  }, []);
+
+  return (
+    <Container>
+      <RotatingLines
+        strokeColor="skyblue"
+        strokeWidth="4"
+        animationDuration="1"
+        width="96"
+        visible={true}
+      />
+      <Message>카카오 계정과 연결 중입니다.</Message>
+    </Container>
+  );
+};
+
+export default UserAuth;
+
+const Container = styled.div`
+  ${props => props.theme.variables.flex('column', 'center', 'center')};
+  ${props => props.theme.variables.absoluteCenter};
+  gap: 30px;
+`;
+
+const Message = styled.div`
+  font-size: 24px;
+`;
